e427f6b7ebcac94f72be9f9831c8f319b4b7a8ae
Cezary <cezary.fib@gmail.com>
2024-07-05 15:43:17 +0200

    Initial commit

---
+++ app/Http/Controllers/NowayController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NowayController extends Controller
+{
+    public function show() {
+        return view('Japikole');
+    }
+}
